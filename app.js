90927997355cdac22c3a6a8b158ef639994d86a3
Tristan <tristan@iMac.local>
2019-09-24 23:12:01 +0200

    gix

--- app.js
+++ app.js
@@ -254,33 +254,24 @@ class Bot {
 
             spawn('raspistill', ['-o', 'cam'+this.picture_i+'.jpg'])
             
-            camera.snap()
-                .then((result) => {
-
-                    Jimp.read('cam0.jpg')
-                        .then(image1 => {
-                            Jimp.read('cam1.jpg')
-                                .then(image2 => {
-                                    var distance = Jimp.distance(image1, image2)
-                                    // var diff = Jimp.diff(image1, image2)
-                                    console.log(distance)
-                                    this.checkMovement()
-                                })
-                                .catch(err => {
-                                    console.log(err)
-                                    this.checkMovement()
-                                })
+            Jimp.read('cam0.jpg')
+                .then(image1 => {
+                    Jimp.read('cam1.jpg')
+                        .then(image2 => {
+                            var distance = Jimp.distance(image1, image2)
+                            // var diff = Jimp.diff(image1, image2)
+                            console.log(distance)
+                            this.checkMovement()
                         })
                         .catch(err => {
                             console.log(err)
                             this.checkMovement()
                         })
-
                 })
-                .catch((error) => {
-                    console.log(error)
+                .catch(err => {
+                    console.log(err)
                     this.checkMovement()
-                });
+                })
 
         }else{
 
